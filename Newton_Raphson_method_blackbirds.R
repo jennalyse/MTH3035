e4c2dbe94dbca81cbdd4ae1e59fa00c8fbb1300e
jennalyse <93606894+jennalyse@users.noreply.github.com>
2023-11-11 14:26:49 +0000

    Update Newton_Raphson_method_blackbirds.R

--- Newton_Raphson_method_blackbirds.R
+++ Newton_Raphson_method_blackbirds.R
@@ -1,3 +1,6 @@
+# Newton Raphson Method for blackbirds
+# Define likelihood function computing matrices ('phia', 'phi1', 'lambda1', 'lambda2') 
+# using logistic function ('expit')
 likelihood_function_matrix <- function(pars_in, Datamatrix) {
   phia <- matrix(c(0, expit(pars_in[2:20])), n1, n2, byrow = TRUE)
   phi1 <- matrix(expit(pars_in[21]), n1, n2, byrow = TRUE)
@@ -6,21 +9,20 @@ likelihood_function_matrix <- function(pars_in, Datamatrix) {
   return(ringlik(Datamatrix, phia, phi1, lambda1, lambda2))
 }
 
-optimize_likelihood <- function(likelihood_function, Datamatrix) {
+# Optimise for the different life stages 
+optimise_likelihood <- function(likelihood_function, Datamatrix) {
   result <- optim(par = initial_pars, fn = likelihood_function, method = "BFGS", Datamatrix = Datamatrix)
   cat("Estimated Parameters:", result$par, "\n")
   return(result)
 }
-
-# For bbpmatrix
+# For pullis
 result_bbpmatrix <- optimize_likelihood(likelihood_function_matrix, Datamatrix)
-
-# For bbjmatrix
+# For juveniles
 result_bbjmatrix <- optimize_likelihood(likelihood_function_matrix, Datamatrixj)
-
-# For bbamatrix
+# For adults
 result_bbamatrix <- optimize_likelihood(likelihood_function_matrix, Datamatrixa)
 
+# Print results
 cat("Estimated Parameters for bbpmatrix:", result_bbpmatrix$par, "\n")
 cat("Estimated Parameters for bbjmatrix:", result_bbjmatrix$par, "\n")
 cat("Estimated Parameters for bbamatrix:", result_bbamatrix$par, "\n")
