6242e2a6f811feadf0327a9e6b9911a2c4253835
jennalyse <93606894+jennalyse@users.noreply.github.com>
2023-11-11 19:40:02 +0000

    Update Linear_model_sandwich_terns.R

--- Linear_model_sandwich_terns.R
+++ Linear_model_sandwich_terns.R
@@ -1,11 +1,10 @@
 # Sandwich data code for linear models
-# sbpmatrix, sbjmatrix, and sbamatrix have already been defined (see Matrices_sandwich_terns)
+
+# sbpmatrix, sbjmatrix, and sbamatrix have already been defined (see Sandwich_tern_matrices)
 sb_data_list <- lapply(list(sbpmatrix, sbjmatrix, sbamatrix), function(matrix) {
   data.frame(years = rep(1970:1990, each = 21), response = as.vector(matrix))
 })
-
 # Linear models
 sb_lm_list <- lapply(sb_data_list, function(data) lm(response ~ years, data = data))
-
-# Summarize results
+# Summarise results
 lapply(sb_lm_list, summary)
