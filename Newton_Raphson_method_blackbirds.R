67ee12f81ff870c69dd9044d7e3b7fd0734fff1d
jennalyse <93606894+jennalyse@users.noreply.github.com>
2023-11-11 14:21:15 +0000

    Create Newton_Raphson_method_blackbirds.R

---
+++ Newton_Raphson_method_blackbirds.R
@@ -0,0 +1,27 @@
+likelihood_function_matrix <- function(pars_in, Datamatrix) {
+  phia <- matrix(c(0, expit(pars_in[2:20])), n1, n2, byrow = TRUE)
+  phi1 <- matrix(expit(pars_in[21]), n1, n2, byrow = TRUE)
+  lambda1 <- matrix(expit(pars_in[22:41]), n1, n2, byrow = TRUE)
+  lambda2 <- matrix(expit(pars_in[22:41]), n1, n2, byrow = TRUE)
+  return(ringlik(Datamatrix, phia, phi1, lambda1, lambda2))
+}
+
+optimize_likelihood <- function(likelihood_function, Datamatrix) {
+  result <- optim(par = initial_pars, fn = likelihood_function, method = "BFGS", Datamatrix = Datamatrix)
+  cat("Estimated Parameters:", result$par, "\n")
+  return(result)
+}
+
+# For bbpmatrix
+result_bbpmatrix <- optimize_likelihood(likelihood_function_matrix, Datamatrix)
+
+# For bbjmatrix
+result_bbjmatrix <- optimize_likelihood(likelihood_function_matrix, Datamatrixj)
+
+# For bbamatrix
+result_bbamatrix <- optimize_likelihood(likelihood_function_matrix, Datamatrixa)
+
+cat("Estimated Parameters for bbpmatrix:", result_bbpmatrix$par, "\n")
+cat("Estimated Parameters for bbjmatrix:", result_bbjmatrix$par, "\n")
+cat("Estimated Parameters for bbamatrix:", result_bbamatrix$par, "\n")
+
